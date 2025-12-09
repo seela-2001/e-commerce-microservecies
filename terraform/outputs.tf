11bfb26b79f60d8acc1c7ddeb502fbb9b7fe228a
Mohamed-Abdalla19 <mohamed.abdullah7419@gmail.com>
2025-12-09 15:22:39 +0200

    add IaC part of cd pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "instance_public_ip" {
 output "ssh_private_key" {
     value = local_file.private_key.filename
 }
+
+resource "local_file" "ansible_inventory" {
+    content  = "[webservers]\n${aws_instance.app_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${local_file.private_key.filename} ansible_become=true"
+    filename = "./hosts.ini"
+}
