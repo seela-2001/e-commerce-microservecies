2bfc0f370363370ddebb820aea734bbf44dad463
Sohila Mustafa <sohila@otomatika.tech>
2025-12-09 22:17:43 +0200

    edited aws region

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
     backend "s3" {
         bucket = "flipkart-tf-state-bucket-gp"
         key = "dev/terraform.tfstate"
-        region = "us-east-1"
+        region = "eu-north-1"
     }
 }
 
