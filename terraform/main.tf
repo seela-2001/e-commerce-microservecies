c4e8469de49f201a294e2746f5cb791270739efb
Sohila Mustafa <sohila@otomatika.tech>
2025-12-09 22:04:40 +0200

    edited bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
 
     backend "s3" {
-        bucket = "flipkart-tf-state-bucket"
+        bucket = "flipkart-tf-state-bucket-gp"
         key = "dev/terraform.tfstate"
         region = "us-east-1"
     }
