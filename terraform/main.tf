e4bbc0644285d17fac7b46cf81a0967087135c85
Sohila Mustafa <sohila@otomatika.tech>
2025-12-10 12:36:59 +0200

    updated region

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
     backend "s3" {
         bucket = "flipkart-tf-state-bucket-gp"
         key = "dev/terraform.tfstate"
-        region = "eu-north-1"
+        region = "us-east-1"
     }
 }
 
