0bcf4f7e833a0154a56bb134fb02d4720ca6db29
Sohila Mustafa <sohila@otomatika.tech>
2025-12-16 11:11:26 +0200

    edited listeners

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,8 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "Allow HTTPS (443) from anywhere"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
