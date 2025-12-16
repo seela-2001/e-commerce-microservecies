2cfd5a55a98627b81fb578a45f7120ffdf0ac7a7
Sohila Mustafa <sohila@otomatika.tech>
2025-12-16 16:28:19 +0200

    removed duplicated ouputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,7 +10,3 @@ resource "local_file" "ansible_inventory" {
     content = "[webservers]\n${join("\n", [for eip in aws_eip.app_server_ip : "${eip.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${local_file.private_key.filename} ansible_become=true"])}"
     filename = "./hosts.ini"
 }
-
-output "instance_public_ip" {
-    value = aws_eip.app_server_ip[*].public_ip 
-}
